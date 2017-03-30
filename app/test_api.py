4166b60766691ce8b22fbb4ceb40362fa5b6bbcf
Peter <pveijk@gmail.com>
2017-03-30 19:21:46 +0000

    more tests

--- app/test_api.py
+++ app/test_api.py
@@ -1,16 +1,13 @@
 # for use with pytest
 import os, requests
 from settings import *
-# from 
-
-data = {
-    "status": ":thumbsup:",
-	"source": "test_api"
-}
+from foobotapi import transformdata
 
 def test_env ():
-    assert fooboturl != 'novalidkey'
+    assert fooboturl.startswith("https://")
     assert foobotkey != 'novalidkey'
+    assert isurl.startswith("https://")
+    assert isbucketkey != 'novalidkey'
     
 def test_get ():
     response = requests.get(fooboturl, headers=fooheaders)
@@ -19,19 +16,19 @@ def test_get ():
     print "headers:"+ str(response.headers)
     print "******************"
     print "content:"+ str(response.text)
-    # check data kind of ok.
     assert response.ok
-    # assert data in response.text
+    assert "datapoints" in response.text
+    assert "sensors" in response.text
     
 def test_transform ():
     data = {
     "status": ":thumbsup:",
 	"source": "test_api"
     }
-    # data = 
-    # r = transformdata(string)
-    # assert data == r
-    assert True
-    
+    x = u'{"uuid":"2701466D278044A0","start":1490861042,"end":1490861042,"sensors":["time","pm","tmp","hum","co2","voc","allpollu"],"units":["s","ugm3","C","pc","ppm","ppb","%"],"datapoints":[[1490861042,6.8000183,18.255,54.493,1062,293,30.800018]]}'
+    y = {u'tmp': 18.255, u'co2': 1062, u'voc': 293, u'hum': 54.493, u'allpollu': 30.800018, u'time': 1490861042, u'pm': 6.8000183}
+    r = transformdata(x)
+    assert x == r
+
 def test_put ():
     assert True
