87bb236ca2c48629e782dc012a6592b2fcff457d
Peter van Eijk <peter@peterhjvaneijk.nl>
2019-11-23 21:41:45 +0100

    logging tweaks

--- app/test_transform.py
+++ app/test_transform.py
@@ -9,7 +9,6 @@ samplefoobot = {"uuid":"2701466D278044A0","start":1571513969,"end":1571513969,"s
 def test_transform ():
     x = u'{"uuid":"2701466D278044A0","start":1490861042,"end":1490861042,"sensors":["time","pm","tmp","hum","co2","voc","allpollu"],"units":["s","ugm3","C","pc","ppm","ppb","%"],"datapoints":[[1490861042,6.8000183,18.255,54.493,1062,293,30.800018]]}'
     y = {u'tmp': 18.255, u'co2': 1062, u'voc': 293, u'hum': 54.493, u'allpollu': 30.800018, u'time': 1490861042, u'pm': 6.8000183}
-    logging.warning("aap")
     r = transformdata(x)
     assert(y == r)
 
